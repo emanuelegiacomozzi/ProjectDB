d9b2d269e2f1ca0f4062c4f79af6387f7c3c59da
emanuelegiacomozzi <emanuelegiacomozzi2018@gmail.com>
2024-11-18 13:32:55 +0100

    esercizio completato

--- server.py
+++ server.py
@@ -18,18 +18,22 @@ try:
 except Exception as e:
     print(f"Errore durante la connessione al database: {e}")
 
+
 def Menu():
 
-    print("Query da eseguire:")
+    print()
+    print("Operazioni disponibili:")
+    print()
     print("1. Quali sono i voli (codice e nome della compagnia) la cui durata supera le 3 ore?")
     print("2. Quali sono le compagnie che hanno voli che superano le 3 ore?")
     print("3. Quali sono i voli (codice e nome della compagnia) che partono dall’aeroporto con codice 'CIA’ ?")
     print("4. Quali sono le compagnie che hanno voli che arrivano all’aeroporto con codice ‘FCO’ ?")
-    print("5. Esci")
+    print("5. Inserimento query ")
+    print("6. Esci")
     
 Menu()
-request = input("Seleziona query da eseguire: ")
-while (request != "5"):
+request = input("Seleziona operazione da eseguire: ")
+while (request != "6"):
     
         if request == "1":
             try:
@@ -40,8 +44,11 @@ while (request != "5"):
                     print(row)
             except:
                 print("Problemi di comunicazione con il server")
+            finally:
+                cursor.close()
+            
 
-        if request == "2":
+        elif request == "2":
             try:
                 cursor = connection.cursor()
                 cursor.execute("SELECT DISTINCT Volo.comp FROM Volo WHERE Volo.durataMinuti > 180")
@@ -50,8 +57,10 @@ while (request != "5"):
                     print(row)
             except:
                 print("Problemi di comunicazione con il server")
+            finally:
+                cursor.close()
 
-        if request == "3":
+        elif request == "3":
             try:
                 cursor = connection.cursor()
                 cursor.execute("SELECT ArrPart.codice, ArrPart.comp FROM ArrPart WHERE ArrPart.partenza = 'CIA'")
@@ -60,8 +69,10 @@ while (request != "5"):
                     print(row)
             except:
                 print("Problemi di comunicazione con il server")
+            finally:
+                cursor.close()
 
-        if request == "4":
+        elif request == "4":
             try:
                 cursor = connection.cursor()
                 cursor.execute("SELECT DISTINCT ArrPart.comp FROM ArrPart WHERE ArrPart.arrivo = 'FCO'")
@@ -70,9 +81,26 @@ while (request != "5"):
                     print(row)
             except:
                 print("Problemi di comunicazione con il server")
+            finally:
+                cursor.close()
         
+        elif request == "5":
+            try:
+                cursor = connection.cursor()
+                query = input("Inserisci la query di lettura: ")
+                cursor.execute(query)
+                rows = cursor.fetchall()
+                for row in rows:
+                    print(row)
+            except:
+                print("Tabella inesistente.Riprova!")
+            finally:
+                cursor.close()
+            
+            
         else:
             print("Inserisci un numero valido!")
     
         Menu()
-        request = input("Seleziona query da eseguire: ")
+        request = input("Seleziona operazione da eseguire: ")
+
