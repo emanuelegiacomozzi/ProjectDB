6408d1709bc000b5440a558bba22bb44038bd032
emanuelegiacomozzi <emanuelegiacomozzi2018@gmail.com>
2024-11-18 10:52:57 +0100

    query al db

--- connessione.py
+++ connessione.py
@@ -17,3 +17,31 @@ try:
     print("Connessione al database avvenuta con successo")
 except Exception as e:
     print(f"Errore durante la connessione al database: {e}")
+
+def Cursor():
+    print("Query: ")
+    print()
+
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM volo\n")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    print()
+
+    cursor2 = connection.cursor()
+    cursor2.execute("SELECT * FROM luogoaeroporto WHERE citta='Roma'")
+    rows2 = cursor2.fetchall()
+    for row in rows2:
+        print(row)
+    print()
+
+    cursor3 = connection.cursor()
+    cursor3.execute("SELECT partenza FROM arrpart")
+    rows3 = cursor3.fetchall()
+    for row in rows3:
+        print(row)
+    print()
+    
+Cursor()
+
