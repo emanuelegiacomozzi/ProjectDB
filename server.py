994e9ee0a85f7f9092f0db4c593c28593f5c73a7
emanuelegiacomozzi <emanuelegiacomozzi2018@gmail.com>
2024-12-05 11:01:51 +0100

    server

--- server.py
+++ server.py
@@ -1,4 +1,8 @@
+from flask import Flask, request , jsonify
+import sys  
 import psycopg2
+
+api = Flask(__name__)
 
 host = "localhost"
 port = "5432"
@@ -6,101 +10,87 @@ dbname="Cielo"
 user="postgres"
 password="postgres"
 
-try:
-    connection = psycopg2.connect(
-        host=host,
-        port=port,
-        dbname=dbname,
-        user=user,
-        password=password
-    )
-    print("Connessione al database avvenuta con successo")
-except Exception as e:
-    print(f"Errore durante la connessione al database: {e}")
+def dbconnect():
+    try:
+        connection = psycopg2.connect(
+            host=host,
+            port=port,
+            dbname=dbname,
+            user=user,
+            password=password
+        )
+        print("Connessione al database avvenuta con successo")
+        return connection
+    except Exception as e:
+        print(f"Errore durante la connessione al database: {e}")
+        sys.exit()
+
+@api.route('/voli', methods=['GET'])
+def GestisciVoli():
+    try:
+        conn = dbconnect()
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM Volo")
+        rows = cursor.fetchall()
+        cursor.close()
+        conn.close()
+        return jsonify(rows)
+    except Exception as e:
+        print(f"Problemi di comunicazione con il server {e}"), 500
 
 
-def Menu():
+@api.route('/compagnie_voli', methods=['GET'])
+def GestisciCompagnieVoli():
+    try:
+        conn = dbconnect()
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM Compagnia c, Volo v WHERE c.nome = v.comp ")
+        rows = cursor.fetchall()
+        cursor.close()
+        conn.close()
+        return jsonify(rows)
+    except Exception as e:
+        print(f"Problemi di comunicazione con il server {e}"), 500
 
-    print()
-    print("Operazioni disponibili:")
-    print()
-    print("1. Quali sono i voli (codice e nome della compagnia) la cui durata supera le 3 ore?")
-    print("2. Quali sono le compagnie che hanno voli che superano le 3 ore?")
-    print("3. Quali sono i voli (codice e nome della compagnia) che partono dall’aeroporto con codice 'CIA’ ?")
-    print("4. Quali sono le compagnie che hanno voli che arrivano all’aeroporto con codice ‘FCO’ ?")
-    print("5. Inserimento query ")
-    print("6. Esci")
-    
-Menu()
-request = input("Seleziona operazione da eseguire: ")
-while (request != "6"):
-    
-        if request == "1":
-            try:
-                cursor = connection.cursor()
-                cursor.execute("SELECT Volo.codice, Volo.comp FROM Volo WHERE Volo.durataMinuti > 180")
-                rows = cursor.fetchall()
-                for row in rows:
-                    print(row)
-            except:
-                print("Problemi di comunicazione con il server")
-            finally:
-                cursor.close()
-            
+@api.route('/aeroporti', methods=['GET'])
+def GestisciAeroporti():
+    try:
+        conn = dbconnect()
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM Aeroporto ")
+        rows = cursor.fetchall()
+        cursor.close()
+        conn.close()
+        return jsonify(rows)
+    except Exception as e:
+        print(f"Problemi di comunicazione con il server {e}"), 500
 
-        elif request == "2":
-            try:
-                cursor = connection.cursor()
-                cursor.execute("SELECT DISTINCT Volo.comp FROM Volo WHERE Volo.durataMinuti > 180")
-                rows = cursor.fetchall()
-                for row in rows:
-                    print(row)
-            except:
-                print("Problemi di comunicazione con il server")
-            finally:
-                cursor.close()
+@api.route('/aeroporti_part_arr', methods=['GET'])
+def GestisciAeroportiPartArr():
+    try:
+        conn = dbconnect()
+        cursor = conn.cursor()
+        cursor.execute("SELECT ap.partenza, ap.arrivo FROM ArrPart ap ")
+        rows = cursor.fetchall()
+        cursor.close()
+        conn.close()
+        return jsonify(rows)
+    except Exception as e:
+        print(f"Problemi di comunicazione con il server {e}"), 500
 
-        elif request == "3":
-            try:
-                cursor = connection.cursor()
-                cursor.execute("SELECT ArrPart.codice, ArrPart.comp FROM ArrPart WHERE ArrPart.partenza = 'CIA'")
-                rows = cursor.fetchall()
-                for row in rows:
-                    print(row)
-            except:
-                print("Problemi di comunicazione con il server")
-            finally:
-                cursor.close()
+@api.route('/luogo_aeroporti', methods=['GET'])
+def GestisciLuogoAeroporti():
+    try:
+        conn = dbconnect()
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM LuogoAeroporto ")
+        rows = cursor.fetchall()
+        cursor.close()
+        conn.close()
+        return jsonify(rows)
+    except Exception as e:
+        print(f"Problemi di comunicazione con il server {e}"), 
 
-        elif request == "4":
-            try:
-                cursor = connection.cursor()
-                cursor.execute("SELECT DISTINCT ArrPart.comp FROM ArrPart WHERE ArrPart.arrivo = 'FCO'")
-                rows = cursor.fetchall()
-                for row in rows:
-                    print(row)
-            except:
-                print("Problemi di comunicazione con il server")
-            finally:
-                cursor.close()
-        
-        elif request == "5":
-            try:
-                cursor = connection.cursor()
-                query = input("Inserisci la query di lettura: ")
-                cursor.execute(query)
-                rows = cursor.fetchall()
-                for row in rows:
-                    print(row)
-            except:
-                print("Tabella inesistente.Riprova!")
-            finally:
-                cursor.close()
-            
-            
-        else:
-            print("Inserisci un numero valido!")
-    
-        Menu()
-        request = input("Seleziona operazione da eseguire: ")
+if __name__ == '__main__':
+    api.run(debug=True, port=5001)
 
